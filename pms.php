ee6e7321cc95f8ab80c8866be005ff655154c71f
BrLi <brli7848@localhost.localdomain>
2014-01-08 17:19:47 +0800

    initial commit

---
+++ pms.php
@@ -0,0 +1,143 @@
+<?php
+
+// Language definitions used by the Private Message System-mod
+$lang_pms = array(
+
+// Common
+'Login required'		=>	'您必須登入才能使用私人訊息系統',
+'Disabled PM'			=>	'您已關閉私人訊息系統',
+'Private Messages'		=>	'私人訊息',
+'PM'					=>	'<acronym title="Private Message">私人訊息</acronym>',
+'Quick message'			=>	'傳送私人訊息',
+'Write message'			=>	'傳送新訊息',
+'Inbox'					=>	'收件匣',
+'Outbox'				=>	'寄件匣',
+'Contacts'				=>	'聯絡人',
+'Delete'				=>	'刪除',
+'Please confirm'		=>	'請確認',
+'New message'			=>	'您有一封未閱讀的私人訊息',
+'New messages'			=>	'您有 %s 封未閱讀的私人訊息',
+'See new'				=>	'閱讀新訊息',
+'See news'				=>	'閱讀新訊息',
+'No new'				=>	'無新訊息',
+'Full boxes'			=>	'您的收件匣已滿',
+'Empty boxes'			=>	'您的收件匣是空的',
+'Full to'				=>	'收件匣已達 %s',
+'Select'				=>	'選取',
+'For select'			=>	'對選取的項目：',
+'Messages'				=>	'訊息',
+'OK'					=>	'OK',
+'PM Menu'				=>	'私人訊息',
+'Sending lists'			=>	'寄件清單',
+
+// List a box
+'Date'					=>	'日期',
+'Subject'				=>	'標題',
+'Sender'				=>	'寄件人',
+'Receiver'				=>	'收件人',
+'Mark as read select'	=>	'標記為已閱讀',
+'Read redirect'			=>	'訊息標記為已閱讀，重新導向中...',
+'Mark as unread select'	=>	'標記為未閱讀',
+'Unread redirect'		=>	'訊息標記為未閱讀，重新導向中...',
+'Mark all'				=>	'標記所有訊息為已閱讀',
+'Read all redirect'		=>	'全部訊息標記為已閱讀，重新導向中...',
+'Must select'			=>	'您必須選取一些訊息',
+'No messages'			=>	'無訊息',
+'Unknown'				=>	'未知',
+
+// View a message
+'View'					=>	'觀看私人討論',
+'Reply'					=>	'回覆',
+'Quote'					=>	'引用',
+'Deleted User'			=>	'刪除使用者',
+'Deleted'				=>	'(已刪除)',
+'With'					=>	'用',
+
+// Send a message
+'Send a message'		=>	'傳送訊息',
+'Send to'				=>	'傳送到',
+'Send multiple'			=>	'You can send the message to several receivers by separating them by commas. Maximum: ',
+'Save message'			=>	'Save message in "Sent" box',
+'Send'					=>	'Send',
+'Sent redirect'			=>	'Messages sent to user, redirecting...',
+'No user'				=>	'There\'s no user with the username "%s".',
+'Dest full'				=>	'%s inbox is full, you can not send you message to this user.',
+'Sender full'			=>	'Can\'t save message, your boxes are full.',
+'Flood'					=>	'At least % seconds have to pass between sends. Please wait a little while and try send the message again.',
+'Must receiver'			=>	'You must give at least one receiver',
+'Too many receiver'		=>	'You can send a message at the same time only to %s receivers maximum.',
+'User blocked'			=>	'%s refuses the private messages.',
+'User disable PM'		=>	'%s disabled the private messages.',
+'Others'				=>	'Others',
+'RE'					=>	'RE: ',
+'Shared message'		=>	'If you have entered more than one receiver, check this box if you want to start a shared discussion (together)',
+
+// Multidelete
+'Multidelete'			=>	'Delete multiple messages',
+'Delete messages comply'=>	'Are you sure you want to delete the selected messages?',
+'Deleted redirect'		=>	'Messages deleted. Redirecting ...',
+
+// Delete
+'Delete message'		=>	'Delete message',
+'Delete message comply'	=>	'Are you sure you want to delete the message?',
+'Del redirect'			=>	'Message deleted, redirecting...',
+'Topic warning info'	=>	'The topic will be deleted from your inbox, but it will stays in the others receivers\' boxes.',
+'Delete for everybody'	=>	'If you tick this checkbox, you will delete the message (or the topic) for all the receivers (available only for admins &amp; mods)',
+
+// Contacts
+'Contacts list'			=>	'Contacts list',
+'Rights contact'		=>	'Contacts rights',
+'Authorized messages'	=>	'Authorized messages',
+'Authorize'				=>	'Authorize',
+'Authorize from'		=>	'Authorize %s to send you private message',
+'Refused messages'		=>	'Refused messages',
+'Refuse'				=>	'Refuse',
+'Refuse from'			=>	'Refused private messages from %s',
+'Refuse user'			=>	'Refuse privates messages coming from this contact',
+'Add to contacts'		=>	'Add to contacts',
+'Add contact'			=>	'Add a contact',
+'Contact name'			=>	'Contact name',
+'User already contact'	=>	'This user is already in your contacts list',
+'User cannot use'		=>	'User can not use private message system.',
+'User not exists'		=>	'User does not seems to exists.',
+'Added contact redirect'=>	'Contact added. Redirecting...',
+'Add'					=>	'Add',
+'Quick message x'		=>	'Send private message to %s',
+'Status redirect'		=>	'Contact status edited. Redirecting...',
+'Multiples status redirect'=>	'Contacts status edited. Redirecting...',
+'No contacts'			=>	'No contact',
+'Must select contacts'	=>	'You must select some contacts',
+
+// Multidelete contacts
+'Multidelete contacts'	=>	'Delete multiple contacts',
+'Delete contacts comply'=>	'Are you sure you want to delete the selected contacts?',
+'Deleted contacts redirect'=>	'Contacts deleted. Redirecting ...',
+
+// Delete contact
+'Delete x'				=>	'Delete contact %s',
+'Delete contact confirm'=>	'Are you sure you want to delete this contact?',
+'Deleted contact redirect'=>	'Contact deleted. Redirecting...',
+
+// Sending lists
+'List name'				=>	'List name',
+'List usernames'		=>	'List usernames',
+'No sending lists'		=>	'No sending lists',
+'Add a list'			=>	'Add a list',
+'Multidelete lists'		=>	'Delete multiple lists',
+'Delete list confirm'	=>	'Are you sure you want to delete this sending list?',
+'Delete lists comply'	=>	'Are you sure you want to delete the selected lists?',
+'Deleted list redirect'	=>	'Sending list deleted. Redirecting...',
+'Must select lists'		=>	'You must select some lists',
+'Select a list'			=>	'Select a list',
+'Delete this list'		=>	'Delete this list',
+'List usernames comma'	=>	'List usernames (separated by commas)',
+'JS required'			=>	'JavaScript is required in order to use the sending lists.',
+'Yourself'				=>	'You cannot add yourself to a sending list.',
+
+// profile.php
+'use_pm_option'			=>	'Enable privates messages system',
+'email_option_infos'	=>	'With this enabled, an e-mail will be sent for all new private message.',
+'email_option'			=>	'Privates messages notification by e-mail',
+'email_option_full'		=>	'Include private messages content',
+
+);
